1f6392d0cf75def3e643f02f5a67d9fb8f2050f5
Ahmad Ayman <mongez.me@gmail.com>
2021-01-20 20:00:16 +0200

    Added : get the active section index while scrolling.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 ///////////////////////////////////////////////////////////
-// Sections
+// data
 //////////////
+
+const sectionList = [
+  { title: "S01 E01", image: "/images/s01_e01.png" },
+  { title: "S01 E02", image: "/images/s01_e02.png" },
+  { title: "S01 E03", image: "/images/s01_e03.png" },
+  { title: "S01 E04", image: "/images/s01_e04.png" },
+];
+
+let activeIndex = 0;
+
+///////////////////////////////////////////////////////////
+// Helper Functions
+//////////////////////
 
 /**  Create a new section and append image to it
  * @param {String} imageURL relative path to the image, or web URL
@@ -15,45 +28,6 @@ const createSectionWithImage = (imageURL, title) => {
   section.appendChild(image);
   return section;
 };
-
-// database
-const sectionList = [
-  { title: "S01 E01", image: "/images/s01_e01.png" },
-  { title: "S01 E02", image: "/images/s01_e02.png" },
-  { title: "S01 E03", image: "/images/s01_e03.png" },
-  { title: "S01 E04", image: "/images/s01_e04.png" },
-];
-
-// create a new fragment to hold the sections
-const sectionDocumentFragment = new DocumentFragment();
-
-// create the page sections for each image in the images list
-for (let i = 0; i < sectionList.length; i++) {
-  const section = sectionList[i];
-  let sectionElement = createSectionWithImage(section.image, section.title);
-  if (i == 0) {
-    sectionElement.classList.add("active_section");
-  }
-  sectionDocumentFragment.appendChild(sectionElement);
-}
-
-// get main tag
-const main = document.querySelector("main");
-
-// append the new sections to the main tag in one batch
-main.append(sectionDocumentFragment);
-
-///////////////////////////////////////////////////////////
-// Nav bar
-///////////
-
-const header = document.querySelector("header");
-const nav = document.createElement("nav");
-const ul = document.createElement("ul");
-ul.setAttribute("class", "main_nav");
-const navDocumentFragment = new DocumentFragment();
-
-nav.appendChild(ul);
 
 /**  Create a new section and append image to it
  * @param {String} imageURL relative path to the image, or web URL
@@ -70,21 +44,54 @@ const createNavbarItem = (url, lable) => {
   return navItem;
 };
 
-const navItemClicked = (index) => {
-  // query the section with this index
-  const sections = document.querySelectorAll("section");
-  const navitems = document.querySelectorAll(".nav_item");
+///////////////////////////////////////////////////////////
+// Sections
+//////////////
 
-  // toggle the active class on it
-  for (let i = 0; i < sections.length; i++) {
-    if (i == index) {
-      sections[i].classList.add("active_section");
-      navitems[i].classList.add("active_nav_item");
-    } else {
-      sections[i].classList.remove("active_section");
-      navitems[i].classList.remove("active_nav_item");
-    }
-    sections[index].scrollIntoView();
+// get main tag
+const main = document.querySelector("main");
+
+// create a new fragment to hold the sections
+const sectionDocumentFragment = new DocumentFragment();
+
+// create the page sections for each image in the images list
+for (let i = 0; i < sectionList.length; i++) {
+  const section = sectionList[i];
+  let sectionElement = createSectionWithImage(section.image, section.title);
+  if (i == 0) {
+    sectionElement.classList.add("active_section");
+  }
+  sectionDocumentFragment.appendChild(sectionElement);
+}
+
+// append the new sections to the main tag in one batch
+main.append(sectionDocumentFragment);
+
+///////////////////////////////////////////////////////////
+// Nav bar
+///////////
+
+const header = document.querySelector("header");
+const nav = document.createElement("nav");
+const ul = document.createElement("ul");
+ul.setAttribute("class", "main_nav");
+nav.appendChild(ul);
+const navDocumentFragment = new DocumentFragment();
+
+const setActiveSection = (index) => {
+  if (index != activeIndex) {
+    // query the section with this index
+    const sections = document.querySelectorAll("section");
+    const navitems = document.querySelectorAll(".nav_item");
+
+    // toggle the active class on it
+    sections[index].classList.add("active_section");
+    navitems[index].classList.add("active_nav_item");
+    sections[activeIndex].classList.remove("active_section");
+    navitems[activeIndex].classList.remove("active_nav_item");
+
+    sections[index].scrollIntoView(true);
+    activeIndex = index;
   }
 };
 
@@ -97,7 +104,7 @@ for (let i = 0; i < sectionList.length; i++) {
   }
   navItem.addEventListener("click", (event) => {
     event.preventDefault();
-    navItemClicked(i);
+    setActiveSection(i);
   });
   navDocumentFragment.appendChild(navItem);
 }
@@ -113,4 +120,39 @@ const backTopBtn = document.querySelector(".back_top");
 backTopBtn.addEventListener("click", () => {
   const header = document.querySelector("header");
   header.scrollIntoView(true);
+});
+
+///////////////////////////////////////////////////////////
+// Active Section on scrolling
+////////////////////////////////
+
+const sections = document.querySelectorAll("section");
+const sectionsList = Array.from(sections);
+
+var isScrolling;
+
+main.addEventListener("scroll", (e) => {
+  // detect if still scrolling
+  window.clearTimeout(isScrolling);
+
+  // run only when use done scrolling
+  isScrolling = window.setTimeout(() => {
+    let index = activeIndex;
+    let minDistance = 0;
+
+    for (let i = 0; i < sections.length; i++) {
+      const section = sections[i];
+      const dist = section.getBoundingClientRect().top;
+      if (Math.abs(dist) <= 10) {
+        index = i;
+        console.log(`section ${i} / dist ${dist}`);
+      }
+    }
+    setActiveSection(index);
+
+    // if (minDistance > 0 && minDistance < 50) {
+    //   setActiveSection(index);
+    // }
+    console.log("------------------------------------------");
+  }, 100);
 });
